49189a4bca07b6f27d7d439c99073d8d67343c82
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-06-06 15:47:11 -0700

    Update something.v

--- something.v
+++ something.v
@@ -1,11 +1,11 @@
-FRAME_2 : 
+FRAME_3 : 
 					begin 
-						// SECTION 2.1 (TOP OF SCREEN)
+						// SECTION 3.1 (TOP OF SCREEN)
 						if (i_vcounter < 135)             
 								r_color <= white;
-						// END SECTION 2.1 (TOP OF SCREEN)
+						// END SECTION 3.1 (TOP OF SCREEN)
 						
-						// SECTION 2.2 (FOREHEAD)
+						// SECTION 3.2 (FOREHEAD)
 						else if (i_vcounter >= 135 && i_vcounter < 180)
 							begin 
 								if (i_hcounter < 324) 
@@ -15,9 +15,9 @@ FRAME_2 :
 								else if (i_hcounter >= 604)
 									r_color <= white; 
 							end  
-						// END SECTION 2.2 (FOREHEAD)
+						// END SECTION 3.2 (FOREHEAD)
 						
-						// SECTION 2.3 (EYES)
+						// SECTION 3.3 (EYES)
 						else if (i_vcounter >= 180 && i_vcounter < 207)
 							begin 
 								if (i_hcounter < 324)
@@ -76,10 +76,10 @@ FRAME_2 :
 									r_color <= yellow;
 								else if (i_hcounter >= 604)
 									r_color <= white;
-              end    
-						// END SECTION 2.3 (EYES)
-						
-						// SECTION 2.4 (MIDDLE OF FACE)
+              end 
+						// END SECTION 3.3 (EYES)
+            
+						// SECTION 3.4 (MIDDLE OF FACE)
 						else if (i_vcounter >= 240 && i_vcounter < 300)
 							begin
 								if (i_hcounter < 324)
@@ -89,10 +89,10 @@ FRAME_2 :
 								else if (i_hcounter >= 604)
 									r_color <= white;
 							end 
-						// END SECTION 2.4 (MIDDLE OF FACE)
+						// END SECTION 3.4 (MIDDLE OF FACE)
 						
-						// SECTION 2.5 (MOUTH)
-						else if (i_vcounter >= 300 && i_vcounter < 315)
+						// SECTION 3.5 (MOUTH)
+            else if (i_vcounter >= 300 && i_vcounter < 315)
 							begin
 								if (i_hcounter < 324)
 									r_color <= white;
@@ -105,10 +105,10 @@ FRAME_2 :
 								else if (i_hcounter >= 604)
 									r_color <= white;
 							end  
-						// END SECTION 2.5 (MOUTH)
-            
-            // SECTION 2.6 (TONUGE)
-            else if (i_vcounter >= 315 && i_vcounter < 317)
+						// END SECTION 3.5 (MOUTH)
+						
+						// SECTION 3.6 (TONUGE)
+            else if (i_vcounter >= 315 && i_vcounter < 320)
               begin 
                 if (i_hcounter < 324)
                   r_color <= white;
@@ -120,7 +120,8 @@ FRAME_2 :
                   r_color <= yellow;
 								else if (i_hcounter >= 604)
 									r_color <= white;
-            else if (i_vcounter >= 317 && i_vcounter < 319)
+              end
+            else if (i_vcounter >= 320 && i_vcounter < 330)
               begin 
                 if (i_hcounter < 324)
                   r_color <= white;
@@ -136,7 +137,8 @@ FRAME_2 :
                   r_color <= yellow;
 								else if (i_hcounter >= 604)
 									r_color <= white;
-            else if (i_vcounter >= 319 && i_vcounter < 321)
+              end 
+            else if (i_vcounter >= 330 && i_vcounter < 340)
               begin 
                 if (i_hcounter < 324)
                   r_color <= white;
@@ -148,10 +150,11 @@ FRAME_2 :
                   r_color <= yellow;
 								else if (i_hcounter >= 604)
 									r_color <= white;
-            // END SECTION 2.6 (TONUGE)
+              end
+            // END SECTION 3.6 (TONUGE)
 						
-						// SECTION 2.7 (CHIN)
-						else if (i_vcounter >= 321 && i_vcounter < 414)
+						// SECTION 3.7 (CHIN)
+						else if (i_vcounter >= 340 && i_vcounter < 414)
 							begin
 								if (i_hcounter < 324)
 									r_color <= white;
@@ -160,10 +163,10 @@ FRAME_2 :
 								else if (i_hcounter >= 604)
 									r_color <= white;
 							end  
-						// END SECTION 2.7 (CHIN)
+						// END SECTION 3.7 (CHIN)
 						
-						// SECTION 2.8 (BOTTOM OF SCREEN)
+						// SECTION 3.8 (BOTTOM OF SCREEN)
 						else if (i_vcounter <= 414)
 							r_color <= white;
-						// END SECTION 2.8 (BOTTOM OF SCREEN)
-					end  // FRAME_2
+						// END SECTION 3.8 (BOTTOM OF SCREEN)
+					end  // FRAME_3
